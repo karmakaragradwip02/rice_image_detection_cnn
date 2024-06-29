e84ca71d932b448bc6d0494ecb86281ad3255b04
Agradwip Karmakar <karmakaragradwip02@gmail.com>
2024-06-29 11:29:33 +0530

    config entity added

---
+++ src/RICE_IMAGE_DETECTION/entity/config_entity.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir = Path
+    source_url = str
+    local_data_file = Path
+    unzip_dir = Path
+
+@dataclass(frozen=True)
+class ModelPreparationConfig:
+    root_dir = Path
+    model_dir = Path
+    weight_decay = float
+    learning_rate = float
+    epsilon = float
+    batch_size = int
+    decay_rate = float
+    classes =int
+
+@dataclass(frozen=True)
+class ModelTrainingConfig:
+    root_dir = Path
+    trained_model_dir = Path
+    training_data = Path
+    epochs = int
+    batch_size =int
+    image_size = list
+
+@dataclass(frozen=True)
+class ModelEvaluation:
+    trained_model_dir = Path
+    training_data: Path
+    all_params: dict
+    mlflow_uri: str
+    image_size: list
+    batch_size: int
