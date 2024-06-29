7026251f4695c363b2dade88885eaa1cc0625245
Agradwip Karmakar <karmakaragradwip02@gmail.com>
2024-06-29 10:41:15 +0530

    folders added

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = "RICE IMAGE DETECTION"
+project_name = "RICE_IMAGE_DETECTION"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
